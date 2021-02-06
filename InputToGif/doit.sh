6a14b7621c44a7f2bf13ac8b50c6982e37fa69f5
Eric Fickes <ericfickes@gmail.com>
2021-02-06 09:19:31 -0700

    Thanks a lot Apple
    Years of audio and microphone powered code down the tubes.  Thanks shit tons Apple.

--- InputToGif/doit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "start"
-/usr/local/bin/gifsicle /Users/ericfickes/Documents/Processing3/InputToGif/frames/*.gif > /Users/ericfickes/Documents/Processing3/InputToGif/out/$1
-#gifsicle /Users/ericfickes/Documents/Processing3/InputToGif/frames/*.gif > out/output.gif
-echo "stop"
--- InputToGif2/doit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "start"
-/usr/local/bin/gifsicle /Users/ericfickes/Documents/Processing3/InputToGif2/frames/*.gif > /Users/ericfickes/Documents/Processing3/InputToGif2/out/$1
-echo "stop"
